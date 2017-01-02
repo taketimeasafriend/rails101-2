fece69bc76a1d53575ad95fd34b9670ca653340f
miaoxiaoge <taketimeasafriend@gmail.com>
2017-01-03 01:57:50 +0800

    add user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -9,6 +11,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
 
     if @group.save
       redirect_to groups_path
@@ -21,27 +24,36 @@ class GroupsController < ApplicationController
    @group = Group.find(params[:id])
  end
 
-  def edit
-   @group = Group.find(params[:id])
-  end
+ def edit
 
-  def update
-    @group = Group.find(params[:id])
-    if @group.update(group_params)
-      redirect_to groups_path, notice: "Update Success"
-    else
-      render :edit
-    end
-  end
+ end
 
-  def destroy
-    @group = Group.find(params[:id])
+ def update
+
+
+   if @group.update(group_params)
+     redirect_to groups_path, notice: "Update Success"
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+
+
    @group.destroy
-   flash[:alert] = "Group deleted"
-   redirect_to groups_path
-  end
+   redirect_to groups_path, alert: "Group deleted"
+ end
 
  private
+
+ def find_group_and_check_permission
+   @group = Group.find(params[:id])
+
+   if current_user != @group.user
+     redirect_to root_path, alert: "You have no permission."
+   end
+ end
 
  def group_params
    params.require(:group).permit(:title, :description)
